70d58bd6df17a0a77d5d42e18eb49c69ede7ed68
alexd <alexd@AlexPC>
2021-05-20 16:37:13 -0500

    ok

---
+++ src/main/java/com/practica/Mvcs01Application.java
@@ -0,0 +1,13 @@
+package com.practica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mvcs01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mvcs01Application.class, args);
+	}
+
+}
